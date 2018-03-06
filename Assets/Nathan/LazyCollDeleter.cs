c50f78a9cc1825f692b59cebd2fda624ed2f6f1f
Nathan <nathanflier@hotmail.com>
2018-03-06 19:18:46 +0100

    added vlaggetje

---
+++ Assets/Nathan/LazyCollDeleter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class LazyCollDeleter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Collider[] colls = GetComponents<Collider> ();
+		for (int i = 0; i < colls.Length; i++) {
+			DestroyImmediate (colls [i]);
+		}
+	}
+}
